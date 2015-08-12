2f3c1344bc118f16f4533a042a29bb1ba5f306ca
Gagandeep Tandon <gagan.tandon@globalpueblosolutions.com>
2015-08-12 11:33:26 +0530

    Added Project

---
+++ DemoAppForCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoAppForCI.Startup))]
+namespace DemoAppForCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
